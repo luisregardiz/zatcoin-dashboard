d9d06160252b73571f7924abb16076384c0dcb56
Luis Garcia Regardiz <80732800+luisregardiz@users.noreply.github.com>
2022-01-22 17:07:59 -0400

    Routes fixed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports ={
 	reactStrictMode: true,
+	exportPathMap: async function (
+		defaultPathMap,
+		{ dev, dir, outDir, distDir, buildId }
+	  ) {
+		return {
+		  '/': { page: '/', query: {title: 'Zatcoin | Dashboard'} },
+		  '/profile': { page: '/profile', query: {title: 'Zatcoin | Profile'} },
+		  '/profile/register': { page: '/profile', query: { title: 'Zatcoin | Register - Profile' } },
+		 
+		}
+	  },
 };
