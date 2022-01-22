5fbc9ba24b3d0fc1cb59553fffd949320934b2e7
Adnan Akbas <klict@users.noreply.github.com>
2022-01-22 19:30:02 +0100

    Implemneted optimizedImages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+// next.config.js
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
 /** @type {import('next').NextConfig} */
-module.exports = {
-    reactStrictMode: true,
-};
+module.exports = withPlugins([
+	[optimizedImages],
+	{
+		reactStrictMode: true,
+	},
+]);
